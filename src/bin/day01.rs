e9a7255306042a746a96ebe202799e597b21dbac
serycjon <jonas@sery.ch>
2024-12-02 17:15:49 +0100

    read day 1 inputs

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,3 +1,13 @@
 fn main() {
-        println!("Hello, Advent of Code!");
+    let contents = std::fs::read_to_string("data/01_input").expect("Failed to read file");
+    
+    for line in contents.lines() {
+        let parts: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().expect("Parse error"))
+            .collect();
+        
+        let (first, second) = (parts[0], parts[1]);
+        println!("First: {}, Second: {}", first, second);
+    }
 }
