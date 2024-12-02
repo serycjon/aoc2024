9169dd660b81d82299ea068157161a9d87965c03
serycjon <jonas@sery.ch>
2024-12-02 17:25:39 +0100

    add day 1 part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,7 @@
 fn main() {
     let contents = std::fs::read_to_string("data/01_input").expect("Failed to read file");
+    let mut xs = Vec::new();
+    let mut ys = Vec::new();
     
     for line in contents.lines() {
         let parts: Vec<i32> = line
@@ -7,7 +9,19 @@ fn main() {
             .map(|s| s.parse().expect("Parse error"))
             .collect();
         
-        let (first, second) = (parts[0], parts[1]);
-        println!("First: {}, Second: {}", first, second);
+	xs.push(parts[0]);
+	ys.push(parts[1]);
+        // let (first, second) = (parts[0], parts[1]);
+        // println!("First: {}, Second: {}", first, second);
     }
+
+    xs.sort();
+    ys.sort();
+
+    let mut sum = 0;
+    for parts in xs.iter().zip(ys.iter()) {
+	sum += (parts.0 - parts.1).abs();
+	// println!("{}", (parts.0 - parts.1).abs());
+    }
+    println!("Part 1: {}", sum);
 }
